4dab94a6421b190352bb0d2b54d063f742273ec8
gaoli11 <gaoli152027@163.com>
2015-12-06 00:18:19 +0800

    提交下载

---
+++ src/com/baidu/download.py
@@ -0,0 +1,13 @@
+# encoding=utf-8
+from selenium import webdriver
+import os
+from time import *
+fp=webdriver.FirefoxProfile()
+fp.set_preference("browser.download.folderList", 2)
+fp.set_preference("browser.download.manager.showWhenStarting",False)
+fp.set_preference("browser.download.dir", os.getcwd())
+fp.set_preference("browser.helperApps.neverAsk.saveToDisk","application/octet-stream")
+driver = webdriver.Firefox(firefox_profile=fp)
+driver.get("http://pypi.Python.org/pypi/selenium")
+driver.find_element_by_partial_link_text("selenium-2").click()
+
