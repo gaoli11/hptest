9291846f1889975c1db659c44c4c9e6b643073cc
gaoli11 <gaoli152027@qq.com>
2015-12-14 14:49:40 +0800

    模块练习提交

--- src/com/test/mytestcase.py
+++ src/com/test/mytestcase.py
@@ -1,8 +1,11 @@
-# encoding = utf-8
+#coding =utf-8
 from selenium import webdriver
 from time import *
 from mydef import Login
+from com.test.mydef import Loginout
 driver=webdriver.Firefox()
 driver.get('http://www.weibo.com/')
 sleep(5)
 Login(driver)
+sleep(10)
+Loginout(driver)
