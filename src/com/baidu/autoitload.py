1895de563ecb326490540646225e5df974bdec9c
gaoli11 <gaoli152027@163.com>
2015-12-29 22:13:52 +0800

    提交主干

--- src/com/baidu/autoitload.py
+++ src/com/baidu/autoitload.py
@@ -3,10 +3,10 @@ from selenium import webdriver
 import os
 from time import *
 driver=webdriver.Firefox()
-filepaths='file:///'+os.path.abspath('upfile.html')
-driver.get(filepaths)
+file='file:///'+os.path.abspath('upfile.html')
+driver.get(file)
 driver.implicitly_wait(10)
 driver.find_element_by_xpath("//input[@type='file']").click()
 os.system(r'.\app\upfile.exe')
-sleep(15)
+sleep(20)
 driver.quit()
